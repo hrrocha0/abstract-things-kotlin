cb81bc17f698081f556e1a15b1c86858c7ec7a07
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2023-02-12 17:03:20 -0300

    Renamed field

--- src/main/kotlin/abstractThings/AbstractThings.kt
+++ src/main/kotlin/abstractThings/AbstractThings.kt
@@ -48,12 +48,12 @@ class WordFrequencyManager : WordFrequencyCounter {
 
 class WordFrequencyController(path: String) {
     private val storage = DataStorageManager(path)
-    private val stopWordManager = StopWordManager()
+    private val stopWordFilter = StopWordManager()
     private val wordFrequencyCounter = WordFrequencyManager()
 
     fun run() {
         storage.words
-            .filter { word -> !stopWordManager.isStopWord(word) }
+            .filter { word -> !stopWordFilter.isStopWord(word) }
             .forEach { word -> wordFrequencyCounter.incrementCount(word) }
 
         wordFrequencyCounter.sorted().forEach { (w, c) -> println("$w - $c") }
